7ee540bd3f23577f01590b79063b285ac4bac5d6
daveget2020@gmail.com <daveget2020@gmail.com>
2024-05-10 16:07:07 +0200

    feat(app): Build a custom navigation menu with st.page_link

--- app.py
+++ app.py
@@ -1,90 +1,24 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from datetime import date, timedelta
-import string
-import time
+from menu import menu
+
+# Initialize st.session_state.role to None
+if "role" not in st.session_state:
+    st.session_state.role = None
+
+# Retrieve the role from Session State to initialize the widget
+st.session_state._role = st.session_state.role
 
 
-@st.cache_data
-def get_data():
-    """Generate random sales data for Widget A through Widget Z"""
-
-    product_names = ["Widget " + letter for letter in string.ascii_uppercase]
-    average_daily_sales = np.random.normal(1_000, 300, len(product_names))
-    products = dict(zip(product_names, average_daily_sales))
-
-    data = pd.DataFrame({})
-    sales_dates = np.arange(date(2023, 1, 1), date(
-        2024, 1, 1), timedelta(days=1))
-    for product, sales in products.items():
-        data[product] = np.random.normal(sales, 300, len(sales_dates)).round(2)
-    data.index = sales_dates
-    data.index = data.index.date
-    return data
+def set_role():
+    # Callback function to save the role selection to Session State
+    st.session_state.role = st.session_state._role
 
 
-@st.experimental_fragment
-def show_daily_sales(data):
-    time.sleep(1)
-    with st.container(height=100):
-        selected_date = st.date_input(
-            "Pick a day ",
-            value=date(2023, 1, 1),
-            min_value=date(2023, 1, 1),
-            max_value=date(2023, 12, 31),
-            key="selected_date",
-        )
-
-    if "previous_date" not in st.session_state:
-        st.session_state.previous_date = selected_date
-    previous_date = st.session_state.previous_date
-    st.session_state.previous_date = selected_date
-    is_new_month = selected_date.replace(day=1) != previous_date.replace(day=1)
-    if is_new_month:
-        st.rerun()
-
-    with st.container(height=510):
-        st.header(f"Best sellers, {selected_date:%B  %Y}")
-        top_ten = data.loc[selected_date].sort_values(ascending=False)[0:10]
-        cols = st.columns([1, 4])
-        cols[0].dataframe(top_ten)
-        cols[1].bar_chart(top_ten)
-
-    with st.container(height=510):
-        st.header(f"Worst sellers, {selected_date:%B  %Y}")
-        bottom_ten = data.loc[selected_date].sort_values()[0:10]
-        cols = st.columns([1, 4])
-        cols[0].dataframe(bottom_ten)
-        cols[1].bar_chart(bottom_ten)
-
-
-def show_monthly_sales(data):
-    time.sleep(1)
-    selected_date = st.session_state.selected_date
-    this_month = selected_date.replace(day=1)
-    next_month = (selected_date.replace(day=28) +
-                  timedelta(days=4)).replace(day=1)
-
-    st.container(height=100, border=True)
-    with st.container(height=510):
-        st.header(f"Daily sales for all products, {this_month:%B %Y}")
-        monthly_sales = data[(data.index < next_month) &
-                             (data.index >= this_month)]
-        st.write(monthly_sales)
-    with st.container(height=510):
-        st.header(f"Total sales for all products, {this_month:%B %Y}")
-        st.bar_chart(monthly_sales.sum())
-
-
-st.set_page_config(layout="wide")
-
-st.title("Daily vs monthly sales, by product")
-st.markdown("This app shows the 2023 daily sales for Widget A through Widget Z.")
-
-data = get_data()
-daily, monthly = st.columns(2)
-with daily:
-    show_daily_sales(data)
-with monthly:
-    show_monthly_sales(data)
+# Selectbox to choose role
+st.selectbox(
+    "Select your role:",
+    [None, "user", "admin", "super-admin"],
+    key="_role",
+    on_change=set_role,
+)
+menu()  # Render the dynamic menu!
