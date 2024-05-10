50a537f52720a7afaceb3b388baf46ea517819fd
daveget2020@gmail.com <daveget2020@gmail.com>
2024-05-10 11:08:24 +0200

    feat(app): Use fragments to run two independent processes separately and Distribute a fragment across multiple containers.

---
+++ stream-live-data.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+
+def get_recent_data(last_timestamp):
+    """Generate and return data from last timestamp to now, at most 60 seconds."""
+    now = datetime.now()
+    if now - last_timestamp > timedelta(seconds=60):
+        last_timestamp = now - timedelta(seconds=60)
